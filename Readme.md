3ee14594d91981aa1edde6ed0631e34fba1b0ad3
karthikeyan rajendran <karthik_ricssion@yahoo.com>
2020-05-15 12:12:16 +0530

    Fixed with - getting mouse positions - Working on transform logics

--- Readme.md
+++ Readme.md
@@ -1,7 +1,13 @@
 ### Working on VanilaJS and CSS
 1. Kscroll.js, a simple pureJS carousel using transform css properties to build
 
+### TODO
+- Work on getting relative position of div
+- Work on logic to translate
+- Check on mobile for touch support
+- Work on different type of cards
+
 Ref \
-https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
-https://github.com/morgansson/g-scrolling-carousel/blob/master/jquery.gScrollingCarousel.js
+https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event \
+https://github.com/morgansson/g-scrolling-carousel/blob/master/jquery.gScrollingCarousel.js \
 https://github.com/morgansson/g-scrolling-carousel/blob/master/jquery.gScrollingCarousel.css
--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,40 +1,122 @@
 (function () {
-    var supportsTouch = false;
-
-    if ('ontouchstart' in window){
-        supportsTouch = true;
-        } else if(window.navigator.msPointerEnabled) {
-        supportsTouch = true;
-        } else if ('ontouchstart' in document.documentElement) {
-        supportsTouch = true;
-    }
-
     var el = document.getElementById('itemsContainer')
+    var innerDoc = document.getElementsByClassName('k-scroll-inner-container')
 
     // SETTINGS FOR CAROUSEL EVENTS
     var isMouseDown = false
+    var containerLeftPos = 0
+
+    // Getting the mouse position
+    function getMousePosition(e, $this) {
+        var m_posx = 0, m_posy = 0, e_posx = 0, e_posy = 0, obj = $this;
+
+        //get mouse position on document crossbrowser
+        // 'mouseposition' - 'parent element position' = 'mouseposition relative to parent element'
+        // https://www.quirksmode.org/js/events_properties.html#position
+        // https://www.quirksmode.org/js/findpos.html
+        if (!e){e = window.event;}
+
+        if (e.pageX || e.pageY){
+            m_posx = e.pageX;
+            m_posy = e.pageY;
+        } else if (e.clientX || e.clientY){
+            m_posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            m_posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+        
+        //get parent element position in document
+        if (obj.offsetParent){
+            do { 
+                e_posx += obj.offsetLeft;
+                e_posy += obj.offsetTop;
+            } while (obj = obj.offsetParent);
+        }
+
+        return {
+            x: m_posx - e_posx,
+            y: m_posy - e_posy,
+            offLef: e_posx
+        }
+    }
+
+    var scrollPos = {
+        mousedown: {
+            x: 0,
+            y: 0,
+            offLef: 0
+        },
+        mousemove: {
+            x: 0,
+            y: 0
+        }
+    }
+
+    function resetScrollPos() {
+        scrollPos = {
+            mousedown: {
+                x: 0,
+                y: 0,
+                offLef: 0,
+                clientX: 0
+            },
+            mousemove: {
+                x: 0,
+                y: 0
+            }
+        }
+    }
+
+    function calculateScrollPos() {
+        // Logic work has to made
+        var dis = 0
+        var symbol = ''
+        if(scrollPos.mousedown.clientX > scrollPos.mousemove.x) {
+            dis = scrollPos.mousedown.clientX - scrollPos.mousemove.x
+            symbol = '-'
+            console.log('mousedown position is greater', scrollPos.mousedown.clientX - scrollPos.mousemove.x )
+        } else {
+            dis = scrollPos.mousemove.x - scrollPos.mousedown.clientX
+            symbol = '+'
+            console.log('mousedown position is lesser', scrollPos.mousemove.x - scrollPos.mousedown.clientX)
+        }
+
+        innerDoc[0].style.transform = 'translateX('+symbol+''+dis+'px)';
+    }
     
     // Setting isMouseDown to true once the user clicks
     // on the el container
     el.addEventListener('mousedown', function(e) {
-        x = e.offsetX;
-        y = e.offsetY;
-
-        console.log(x, y)
-
+        var data = getMousePosition(e, this)
+        scrollPos.mousedown.x = data.x
+        scrollPos.mousedown.y = data.y
+        scrollPos.mousedown.offLef = data.offLef
+        scrollPos.mousedown.clientX = data.x + data.offLef
         isMouseDown = true
     })
 
-    el.addEventListener('mousemove', function(e) {
+    window.addEventListener('mousemove', function(e) {
         if(isMouseDown) {
-            console.log('Am clicked and dragging')
+            innerDoc[0].classList.add("no-select");
+            var data = getMousePosition(e, this)
+            scrollPos.mousemove.x = data.x
+            scrollPos.mousemove.y = data.y
+            calculateScrollPos()
         }
     })
 
-    el.addEventListener('mouseup', function(e) {
+    window.addEventListener('mouseup', function(e) {
         if(isMouseDown) {
             isMouseDown = false
+            innerDoc[0].classList.remove("no-select");
             console.log('Am setting it to false')
+            resetScrollPos()
         }
+    })
+
+    document.querySelectorAll('.k-scroll-item').forEach(item => {
+        item.addEventListener('click', event => {
+            event.stopPropagation()
+            console.log('I am clicked!')
+        })
     })
 })();
